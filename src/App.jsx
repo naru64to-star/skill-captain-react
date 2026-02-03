1895e061c9121573b16dd6d85c915ace36b66fee
rajni <naru64to-star>
2026-02-03 23:36:29 +0530

    Initial commit for day 1 project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+function App(){
+    return(
+        <main>
+     <h1>hello world </h1>
+     <h2>hey that my first website</h2>
+
+     <p>explore this website and share your experience</p>
+     <div>
+        box 1
+     </div>
+<b>wonderful experience</b>
+<br></br>
+
+<i>good morning</i>
+<ol>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>JavaScript</li>
+</ol>
+
+<ul>
+  <li>Apple</li>
+  <li>Banana</li>
+  <li>Mango</li>
+</ul>
+
+        </main>
+    )
+}
+
+export default App;
