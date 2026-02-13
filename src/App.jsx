c20cc192f9ae6c0b5425beff787a9bae197eab1e
rajni <naru64to-star>
2026-02-13 16:30:27 +0530

    click

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
  import { useEffect, useState } from "react";
  export default function App(){
-     const[count,setCount]=useState(0);
-     const[start,setStart]=useState(false);
-
-     const autoStart=()=>{setStart(true);}
-
-     useEffect(()=>{
-        if(!start)return;
-        const timer=setInterval(()=>{
-            setCount(count=>count+1);
-        },1000)
-        return ()=>clearInterval(timer);
-     },[start]);
-    
+    const[show, setShow] = useState(false);
     return(
+    
         <>
- <h1>Count{count}</h1>
-   <button onClick={autoStart}>Click increas me</button>
-   <br></br>
+        <h1>day 5</h1>
+   <button onClick={()=>setShow(!show)}>Click increas me</button>
+   
+   {show &&(
+        <>
+        <p> thanks for click me</p>
+        <h1>welcome here</h1>
+        </>
+    )
+   }
+
+   <h3>welcome my website</h3>
    
         
         </>
